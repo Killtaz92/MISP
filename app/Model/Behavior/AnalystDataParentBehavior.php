79cc2bf1993899046ea92f7d3999b6e4f9792531
iglocska <andras.iklody@gmail.com>
2024-12-31 00:29:18 +0100

    fix: [analystdata] fixes for analyst data without any nesting failing

--- app/Model/Behavior/AnalystDataParentBehavior.php
+++ app/Model/Behavior/AnalystDataParentBehavior.php
@@ -44,8 +44,9 @@ class AnalystDataParentBehavior extends ModelBehavior
         foreach ($types as $type) {
             $this->{$type} = ClassRegistry::init($type);
             $this->{$type}->fetchRecursive = $fetchRecursive;
+            debug($type . ':' . $object['uuid']);
             $temp = $this->{$type}->fetchForUuid($object['uuid'], $this->__currentUser);
-            if (!empty($temp[$type])) {
+            if (!empty($temp)) {
                 foreach ($temp as $k => $temp_element) {
                     $data[$type][] = $temp_element[$type];
                     $childNotesAndOpinions = $this->{$type}->fetchChildNotesAndOpinions($this->__currentUser, $temp_element[$type], $this->__isRest);
